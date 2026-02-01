1e9a658d43d79a5635ae4d4ebb615ce380d04743
focfoc <tmddus7010@gmail.com>
2026-02-01 18:54:59 +0900

    test: 통합 테스트에서 CRUD DB 반영 검증 로직 명시화

--- src/test/java/com/example/board/article/service/ArticleServiceIntegrationTest.java
+++ src/test/java/com/example/board/article/service/ArticleServiceIntegrationTest.java
@@ -2,6 +2,7 @@ package com.example.board.article.service;
 
 import com.example.board.article.exception.ArticleNotFoundException;
 import com.example.board.domain.Article;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,9 @@ public class ArticleServiceIntegrationTest {
     @Autowired
     private ArticleService articleService;
 
+    @Autowired
+    EntityManager em;
+
     @Test
     @DisplayName("게시글 저장 후 게시글 저장 확인")
     void saveArticle_success() {
@@ -29,6 +33,8 @@ public class ArticleServiceIntegrationTest {
         Long id = article.getArticleNo();
 
         //then
+        em.flush();
+        em.clear();
         assertThat(id).isNotNull();
         Article resultArticle = articleService.findById(id);
         assertThat(resultArticle).isNotNull();
@@ -48,6 +54,8 @@ public class ArticleServiceIntegrationTest {
         articleService.update(id, "테스트를 수정했습니다.");
 
         //then
+        em.flush();
+        em.clear();
         Article resultArticle = articleService.findById(id);
         assertThat(resultArticle.getTitle()).isEqualTo("테스트를 수정했습니다.");
         assertThat(resultArticle.getViewCount()).isEqualTo(0L);
@@ -67,6 +75,8 @@ public class ArticleServiceIntegrationTest {
         articleService.delete(id);
 
         //then
+        em.flush();
+        em.clear();
         assertThatThrownBy(() -> articleService.findById(id))
                 .isInstanceOf(ArticleNotFoundException.class);
 
